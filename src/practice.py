3ff8520893d75212dd8e6770faa8088d17173618
Nabilah Zieha <nabbyz95@gmail.com>
2025-05-23 16:18:58 +0800

    modified practice

--- src/practice.py
+++ src/practice.py
@@ -1,1 +1,22 @@
-print("Hello World")
+spam_amount = 0
+print(spam_amount)
+
+# ordering Spam, egg, Spam, Spam, Bacon and Spam (4 more servings of Spam)
+spam_amount = spam_amount + 4
+
+if spam_amount > 0:
+    print("But I don't want ANY spam!")
+
+viking_song = "Spam " * spam_amount
+print(viking_song)
+
+type(spam_amount)
+
+type(19.95)
+
+hat_height_cm = 25
+my_height_cm = 190
+
+# how tall am I, in metres, when wearing my hat ?
+total_height_meters = (hat_height_cm + my_height_cm) / 100
+print("Height in meters =", total_height_meters)
